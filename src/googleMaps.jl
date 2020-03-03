aa17cfe95c65354b8ad966c1a32a235c85a4ea5b
Nikola Stoyanov <nstoyanov.92@gmail.com>
2020-03-03 09:14:09 +0000

    Add start and finish markers to map plot

--- src/googleMaps.jl
+++ src/googleMaps.jl
@@ -1,11 +1,21 @@
 function mapsURL(path::String; type="terrain", token::String=ENV["GOOGLE_MAPS_API"],
-                 size=800, scale=2, mapMarkersStart="", mapMarkersEnd="")
+                 size=800, scale=2, MarkersStart=(0.0, 0.0), MarkersEnd=(0.0, 0.0))
     mapURL = "https://maps.googleapis.com/maps/api/staticmap?maptype="
     mapType = type
     mapPath = "&path=enc:" * path * "&"
     mapKey = "key=$token" * "&"
     mapSize = "size=$size" * "x$size" * "&"
     mapScale = "scale=$scale" * "&"
+
+    mapMarkersStart = ""
+    if MarkersStart != (0.0, 0.0)
+        mapMarkersStart = "color:yellow|label:S|$(MarkersStart[1]),$(MarkersStart[2])"
+    end
+
+    mapMarkersEnd = ""
+    if MarkersEnd != (0.0, 0.0)
+        mapMarkersEnd = "&color:green|label:F|$(MarkersEnd[1]),$(MarkersEnd[2])"
+    end
 
     return mapURL * mapType * mapPath * mapKey * mapSize * mapScale *
            mapMarkersStart * mapMarkersEnd
